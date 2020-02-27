44728efa00e9fd3e9cc85c6bae769c7ca55e6224
martyminion <martkimwaweru@gmail.com>
2020-02-27 11:05:39 +0300

    Working address book logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
+}
+
+Contact.prototype.fullName = function(){
+  return this.firstName + " " +this.lastName;
 }
 
 // user interface logic
@@ -15,8 +20,13 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName +" "+ newContact.lastName + "</span></li>");
-    
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });  
 
     
   });
